fb575bac2d67c5a1a39b30f3316806a18d74cc89
Sofiia <65614017+Wozniak456@users.noreply.github.com>
2021-03-25 00:45:04 +0200

    Add project files.

---
+++ Laba3_OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Library_OOP_3;
+namespace Laba3_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input an array : ");
+            string str = Console.ReadLine();
+            Class1 instance = new Class1(str.ToCharArray());
+            Console.WriteLine($"\nThe amount of vowels: {instance.AmountOfVowels}\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("input an index: ");
+                int index = int.Parse(Console.ReadLine());
+                Console.WriteLine(instance[index]);
+            }
+        }
+    }
+}
---
+++ Library_OOP_3/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Library_OOP_3
+{
+    public class Class1
+    {
+        private char[] _array;
+        private int _size;
+        private int _amountOfVowels; //кількість голосних букв
+        public Class1(char[] array) //конструктор для початкового запису введеного масиву у закрите поле _array
+        {
+            _size = array.Length;
+            _array = new char[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                _array[i] = array[i];
+            }
+        }
+        public int AmountOfVowels // властивість, яка містить кількість голосних букв у масиві
+        {
+            get
+            {
+                int counter = 0;
+                for (int i = 0; i < _size; i++)
+                {
+                    char.ToLower(_array[i]);
+                    if ("aeouyi".Contains(_array[i]))
+                    {
+                        counter++;
+                    }
+                }
+                _amountOfVowels = counter;
+                return _amountOfVowels;
+            }
+        }
+        public char this[int index] //індексатор, який у випадку зчитання невалідного індексу пропонує ще раз ввести індекс 
+        {
+            get 
+            {
+                try
+                {
+                    return char.ToUpper(_array[index]);
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    Console.WriteLine("please, enter a valid index");
+                    return '\0';
+                }
+            }
+            set 
+            {
+                _array[index] = value;
+            }
+        }
+    }
+}
